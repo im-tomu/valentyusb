d49ff54087df9536f1242426d77800da1f08ac22
Sean Cross <sean@xobs.io>
2019-02-08 21:38:22 +0900

    usbcore: rx: refactor crc_test to one-test-per-function
    Signed-off-by: Sean Cross <sean@xobs.io>

--- valentyusb/usbcore/rx/crc_test.py
+++ valentyusb/usbcore/rx/crc_test.py
@@ -9,7 +9,7 @@ from ..test.common import BaseUsbTestCase
 from .crc import RxCrcChecker
 
 class TestRxCrcChecker(BaseUsbTestCase):
-    def test_shifter(self):
+    def shifter_test(self, i, vector):
         def send(reset, valid, value):
             crc_good = ""
             for i in range(len(valid)):
@@ -29,7 +29,33 @@ class TestRxCrcChecker(BaseUsbTestCase):
 
             return crc_good
 
-        test_vectors = [
+
+        def stim(width, polynomial, initial, residual, reset, valid, value, crc_good):
+            actual_crc_good = yield from send(reset, valid, value)
+            self.assertEqual(actual_crc_good, crc_good)
+
+        with self.subTest(i=i, vector=vector):
+            i_valid = Signal()
+            i_data = Signal()
+            i_reset = Signal()
+
+            dut = RxCrcChecker(
+                vector["width"],
+                vector["polynomial"],
+                vector["initial"],
+                vector["residual"],
+                i_valid,
+                i_data,
+                i_reset)
+
+            run_simulation(
+                dut,
+                stim(**vector),
+                vcd_name=self.make_vcd_name(testsuffix=str(i)),
+            )
+
+    def test_usb2_token_with_good_crc5(self):
+        return self.shifter_test(0, 
             dict(
                 # USB2 token with good CRC5 (1)
                 width       = 5,
@@ -40,8 +66,11 @@ class TestRxCrcChecker(BaseUsbTestCase):
                 valid       = "_----------------___",
                 value       = "00000000000001000000",
                 crc_good    = "_______-__________--"
-            ),
+            )
+        )
 
+    def test_usb2_token_with_good_crc5_and_pipeline_stalls(self):
+        return self.shifter_test(1,
             dict(
                 # USB2 token with good CRC5 and pipeline stalls (1)
                 width       = 5,
@@ -52,8 +81,11 @@ class TestRxCrcChecker(BaseUsbTestCase):
                 valid       = "_-___-___------------___-___-___",
                 value       = "00000011100000000001011100000000",
                 crc_good    = "_____________-________________--"
-            ),
+            )
+        )
 
+    def test_usb2_token_with_bad_crc5(self):
+        return self.shifter_test(2,
             dict(
             # USB2 token with bad CRC5 (1)
                 width       = 5,
@@ -64,8 +96,11 @@ class TestRxCrcChecker(BaseUsbTestCase):
                 valid       = "_----------------___",
                 value       = "00010000000001000000",
                 crc_good    = "______-________-____"
-            ),
+            )
+        )
 
+    def test_usb2_token_with_good_crc5_2(self):
+        return self.shifter_test(3,
             dict(
                 # USB2 token with good CRC5 (2)
                 width       = 5,
@@ -76,8 +111,11 @@ class TestRxCrcChecker(BaseUsbTestCase):
                 valid       = "_----------------___",
                 value       = "00000011011011101000",
                 crc_good    = "_______-__________--"
-            ),
+            )
+        )
 
+    def test_usb2_token_with_bad_crc5_2(self):
+        return self.shifter_test(4,
             dict(
                 # USB2 token with bad CRC5 (2)
                 width       = 5,
@@ -88,8 +126,11 @@ class TestRxCrcChecker(BaseUsbTestCase):
                 valid       = "_----------------___",
                 value       = "00010011011011101000",
                 crc_good    = "______-_____________"
-            ),
+            )
+        )
 
+    def test_usb2_token_with_good_crc5_1_2(self):
+        return self.shifter_test(5,
             dict(
                 # Two USB2 token with good CRC5 (1,2)
                 width       = 5,
@@ -100,8 +141,11 @@ class TestRxCrcChecker(BaseUsbTestCase):
                 valid       = "_----------------_________----------------___",
                 value       = "000000000000010000000000000000011011011101000",
                 crc_good    = "_______-__________---------_____-__________--"
-            ),
+            )
+        )
 
+    def test_usb2_data_with_good_crc16(self):
+        return self.shifter_test(6,
             dict(
                 # USB2 data with good CRC16 (1)
                 width       = 16,
@@ -112,8 +156,11 @@ class TestRxCrcChecker(BaseUsbTestCase):
                 valid       = "_--------_--------_--------_--------_--------_--------_--------_--------_----------------______",
                 value       = "00000000100110000000000000001000000000000000000000000000000001000000000001011101100101001000010",
                 crc_good    = "__________________________________________________________________________________________-----"
-            ),
+            )
+        )
 
+    def test_usb2_data_with_bad_crc16(self):
+        return self.shifter_test(7,
             dict(
                 # USB2 data with bad CRC16 (1)
                 width       = 16,
@@ -124,34 +171,8 @@ class TestRxCrcChecker(BaseUsbTestCase):
                 valid       = "_--------_--------_--------_--------_--------_--------_--------_--------_----------------______",
                 value       = "00000000100110000000000000001000000000010000000000000000000001000000000001011101100101001000010",
                 crc_good    = "_______________________________________________________________________________________________"
-            ),
-        ]
-
-        def stim(width, polynomial, initial, residual, reset, valid, value, crc_good):
-            actual_crc_good = yield from send(reset, valid, value)
-            self.assertEqual(actual_crc_good, crc_good)
-
-        for i, vector in enumerate(test_vectors):
-            with self.subTest(i=i, vector=vector):
-                i_valid = Signal()
-                i_data = Signal()
-                i_reset = Signal()
-
-                dut = RxCrcChecker(
-                    vector["width"],
-                    vector["polynomial"],
-                    vector["initial"],
-                    vector["residual"],
-                    i_valid,
-                    i_data,
-                    i_reset)
-
-                run_simulation(
-                    dut,
-                    stim(**vector),
-                    vcd_name=self.make_vcd_name(testsuffix=str(i)),
-                )
-
+            )
+        )
 
 if __name__ == "__main__":
     unittest.main()
