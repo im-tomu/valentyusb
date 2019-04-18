6ec01765c1b8b6ab00c01d777ee298cc8166c826
Sean Cross <sean@xobs.io>
2019-04-18 21:30:02 +0800

    cpu: usbwishbonebridge: fix "read", work on "write"
    Signed-off-by: Sean Cross <sean@xobs.io>

--- valentyusb/usbcore/cpu/usbwishbonebridge.py
+++ valentyusb/usbcore/cpu/usbwishbonebridge.py
@@ -43,8 +43,10 @@ class USBWishboneBridge(Module):
         # True when the "sink" value has data
         self.sink_valid = Signal()
 
+        self.send_ack = Signal()
+
         # Indicates whether a "debug" packet is currently being processed
-        self.debug_in_progress = Signal()
+        self.n_debug_in_progress = Signal()
 
         address = Signal(32, reset_less=True)
         address_ce = Signal()
@@ -64,7 +66,7 @@ class USBWishboneBridge(Module):
             If(cmd_ce, cmd.eq(usb_core.data_recv_payload[7:8])),
             If(address_ce, address.eq(Cat(address[8:32], usb_core.data_recv_payload))),
             If(rx_data_ce,
-                data.eq(Cat(usb_core.data_recv_payload, data[0:24]))
+                data.eq(Cat(data[8:32], usb_core.data_recv_payload))
             ).Elif(tx_data_ce,
                 data.eq(self.wishbone.dat_r)
             )
@@ -79,13 +81,12 @@ class USBWishboneBridge(Module):
         #     fsm.reset.eq(timer.done)
         # ]
         fsm.act("IDLE",
-            NextValue(self.debug_in_progress, 0),
+            self.n_debug_in_progress.eq(1),
             If(usb_core.data_recv_put,
                 If(usb_core.tok == PID.SETUP,
                     # If we get a SETUP packet with a "Vendor" type
                     # going to this device, treat that as a DEBUG packet.
                     If(usb_core.data_recv_payload[0:7] == 0x40,
-                        NextValue(self.debug_in_progress, 1),
                         cmd_ce.eq(1),
                         NextState("RECEIVE_ADDRESS"),
                     )
@@ -112,19 +113,38 @@ class USBWishboneBridge(Module):
                 If(cmd,
                     NextState("READ_DATA")
                 ).Else(
-                    NextState("WAIT_PKT_END")
-                    # NextState("RECEIVE_DATA")
+                    NextState("RECEIVE_DATA")
                 ),
             ),
         )
+
         fsm.act("RECEIVE_DATA",
             If(usb_core.data_recv_put,
                 rx_data_ce.eq(1),
                 byte_counter_ce.eq(1),
                 If(byte_counter == 3,
-                    NextState("WRITE_DATA"),
+                    NextState("WAIT_RECEIVE_DATA_END"),
                     byte_counter_reset.eq(1)
                 )
+            )
+        )
+        fsm.act("WAIT_RECEIVE_DATA_END",
+            self.send_ack.eq(1),
+            If(usb_core.end,
+                NextState("ACK_DATA_RECEIVED")
+            )
+        )
+        fsm.act("ACK_DATA_RECEIVED",
+            self.send_ack.eq(1),
+            If(usb_core.end,
+                NextState("ACK_DATA_RECEIVED"),
+            ),
+        )
+
+        fsm.act("ACK_DATA_RECEIVED",
+            self.send_ack.eq(1),
+            If(usb_core.end,
+                NextState("WRITE_DATA")
             )
         )
         self.comb += [
@@ -140,32 +160,28 @@ class USBWishboneBridge(Module):
             self.wishbone.we.eq(1),
             self.wishbone.cyc.eq(1),
             If(self.wishbone.ack,
-                NextState("WAIT_USB_OUT_ACK")
+                NextState("WAIT_SEND_ACK"),
             )
         )
-        fsm.act("WAIT_USB_OUT_ACK",
-            self.sink_valid.eq(1),
-            If(usb_core.start,
-                NextState("WAIT_USB_OUT_ACK_END"),
-            )
-        )
-        fsm.act("WAIT_USB_OUT_ACK_END",
+        fsm.act("WAIT_SEND_ACK",
+            self.send_ack.eq(1),
             If(usb_core.end,
-                NextState("WAIT_USB_IN_ACK")
+                NextState("WAIT_PKT_END_DBG"),
+            ),
+        )
+        fsm.act("WAIT_PKT_2",
+            self.send_ack.eq(1),
+            If(usb_core.end,
+                NextState("WAIT_PKT_END_DBG"),
             )
         )
-        fsm.act("WAIT_USB_IN_ACK",
-            self.sink_valid.eq(1),
-            If(usb_core.start,
-                NextState("WAIT_PKT_END")
-            )
+        fsm.act("WAIT_PKT_3",
+            self.send_ack.eq(1),
+            If(usb_core.end,
+                NextState("WAIT_PKT_END_DBG"),
+            ),
         )
-        fsm.act("WAIT_USB_ACK",
-            self.sink_valid.eq(1),
-            If(usb_core.start,
-                NextState("WAIT_PKT_END")
-            )
-        )
+
         fsm.act("READ_DATA",
             self.wishbone.stb.eq(1),
             self.wishbone.we.eq(0),
@@ -183,14 +199,21 @@ class USBWishboneBridge(Module):
             If(usb_core.data_send_get,
                 byte_counter_ce.eq(1),
                 If(byte_counter == 3,
-                    NextState("WAIT_PKT_END")
+                    NextState("WAIT_PKT_END_DBG")
                 )
             )
         )
 
-        fsm.act("WAIT_PKT_END",
+        fsm.act("WAIT_PKT_END_DBG",
+            self.send_ack.eq(1),
             If(usb_core.end,
-                NextValue(self.debug_in_progress, 0),
+                NextState("IDLE"),
+            )
+        )
+
+        fsm.act("WAIT_PKT_END",
+            self.n_debug_in_progress.eq(1),
+            If(usb_core.end,
                 NextState("IDLE"),
             )
         )
