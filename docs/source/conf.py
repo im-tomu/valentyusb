d68ad8242ae45777addc7c9e292aef5d0f752b40
Sean Cross <sean@xobs.io>
2019-09-04 14:57:07 +0800

    docs: don't let sphinx reorder our classes
    Signed-off-by: Sean Cross <sean@xobs.io>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,6 +53,9 @@ extensions = [
     'sphinxcontrib.wavedrom',
 ]
 
+# Prevent sphinx from reordering our classes
+autodoc_member_order = 'bysource'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
