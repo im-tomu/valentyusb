e4394f79f502ae4495520956b33363cf6cd16fd9
Sean Cross <sean@xobs.io>
2019-09-07 09:29:05 +0800

    rx: pipeline: increase reset counter to 60 cycles
    There might have been an off-by-some error somewhere, and as a
    result we were resetting the USB controller during the middle
    of a transaction sometimes.
    
    Increase the cycle counter to 60 cycles in order to combat this.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- valentyusb/usbcore/rx/pipeline.py
+++ valentyusb/usbcore/rx/pipeline.py
@@ -35,12 +35,12 @@ class RxPipeline(Module):
         # A reset condition is one where the device is in SE0 for more
         # than 2.5 uS, which is ~30 clock cycles as 12 MHz.
         self.o_reset = Signal()
-        reset_counter = Signal(6)
-        self.comb += self.o_reset.eq(reset_counter[5])
+        reset_counter = Signal(7)
+        self.comb += self.o_reset.eq(reset_counter[6])
         self.sync.usb_12 += [
             If(self.i_usbp | self.i_usbn,
                 reset_counter.eq(0),
-            ).Elif(~reset_counter[5],
+            ).Elif(~reset_counter[6],
                 reset_counter.eq(reset_counter + 1),
             )
         ]
