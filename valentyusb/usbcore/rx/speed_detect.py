bd3cd1e06391cad5814a286c0a807b259960f788
Marcus Comstedt <marcus@mc.pp.se>
2022-05-01 21:54:52 +0200

    Add pull-up detection for host mode

---
+++ valentyusb/usbcore/rx/speed_detect.py
@@ -0,0 +1,37 @@
+from migen import *
+
+from migen.fhdl.decorators import ResetInserter
+
+@ResetInserter()
+class RxSpeedDetect(Module):
+
+    def __init__(self, threshold=1000):
+        self.i_d_p = Signal()
+        self.i_d_n = Signal()
+        self.i_tx_en = Signal()
+        self.o_ls_detect = Signal()
+        self.o_fs_detect = Signal()
+
+        cnt = Signal(max=threshold+1)
+        pull_mode_cur = Signal(2)
+        pull_mode_last = Signal(2)
+        pull_mode_latched = Signal(2)
+
+        self.comb += [
+            self.o_fs_detect.eq(pull_mode_latched[0]),
+            self.o_ls_detect.eq(pull_mode_latched[1])
+        ]
+
+        self.sync += [
+            pull_mode_last.eq(pull_mode_cur),
+            pull_mode_cur.eq(Cat(self.i_d_p, self.i_d_n)),
+            If (self.i_tx_en | (pull_mode_cur != pull_mode_last) |
+                (pull_mode_last == 0b11) |
+                ((pull_mode_last ^ pull_mode_latched) == 0b11),
+                cnt.eq(0)
+            ).Elif (cnt == threshold,
+                pull_mode_latched.eq(pull_mode_last)
+            ).Else (
+                cnt.eq(cnt + 1)
+            )
+        ]
